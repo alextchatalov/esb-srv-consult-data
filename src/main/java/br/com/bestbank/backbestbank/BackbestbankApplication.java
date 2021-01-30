1ca9d41ad2d713f482f22f35fa216008c48be491
Alex Travagin Chatalov <alex_tr1992@hotmail.com>
2021-01-30 10:36:00 -0300

    initial commit

---
+++ src/main/java/br/com/bestbank/backbestbank/BackbestbankApplication.java
@@ -0,0 +1,13 @@
+package br.com.bestbank.backbestbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackbestbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackbestbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bestbank/backbestbank/BackbestbankApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bestbank.backbestbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackbestbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
